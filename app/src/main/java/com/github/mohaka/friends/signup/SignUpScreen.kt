df43f42b6b68f4c3f4a440b63a7c96af5d81a184
Haytham KATBY <mohakapt@gmail.com>
2023-01-12 18:38:54 +0300

    Implement sign up screen field interactions

--- app/src/main/java/com/github/mohaka/friends/signup/SignUpScreen.kt
+++ app/src/main/java/com/github/mohaka/friends/signup/SignUpScreen.kt
@@ -1,13 +1,15 @@
 package com.github.mohaka.friends.signup
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,30 +23,21 @@ fun SignUpScreen() {
 			.fillMaxSize()
 			.padding(16.dp)
 	) {
-		Row(
-			modifier = Modifier.fillMaxWidth(),
-			horizontalArrangement = Arrangement.Center
-		) {
-			Text(
-				text = stringResource(R.string.text_createAccount),
-				style = typography.h4
-			)
-		}
+		ScreenTitle(R.string.text_createAccount)
 
 		Spacer(modifier = Modifier.height(16.dp))
 
-		OutlinedTextField(
-			modifier = Modifier.fillMaxWidth(),
-			value = "",
-			label = { Text(text = stringResource(id = R.string.hint_email)) },
-			onValueChange = {}
+		var email by remember { mutableStateOf("") }
+		var password by remember { mutableStateOf("") }
+
+		EmailField(
+			value = email,
+			onValueChange = { email = it }
 		)
 
-		OutlinedTextField(
-			modifier = Modifier.fillMaxWidth(),
-			value = "",
-			label = { Text(text = stringResource(id = R.string.hint_password)) },
-			onValueChange = {}
+		PasswordField(
+			value = password,
+			onValueChange = { password = it }
 		)
 
 		Spacer(modifier = Modifier.height(16.dp))
@@ -53,6 +46,50 @@ fun SignUpScreen() {
 			modifier = Modifier.fillMaxWidth(),
 			onClick = { },
 			content = { Text(text = stringResource(id = R.string.action_signUp)) }
+		)
+	}
+}
+
+@Composable
+private fun EmailField(value: String, onValueChange: (String) -> Unit) {
+	OutlinedTextField(
+		modifier = Modifier.fillMaxWidth(),
+		value = value,
+		label = { Text(text = stringResource(id = R.string.hint_email)) },
+		onValueChange = onValueChange
+	)
+}
+
+@Composable
+private fun PasswordField(value: String, onValueChange: (String) -> Unit) {
+	var isPasswordVisible by remember { mutableStateOf(false) }
+
+	OutlinedTextField(
+		modifier = Modifier.fillMaxWidth(),
+		value = value,
+		trailingIcon = {
+			IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+				Icon(
+					painter = painterResource(id = if (isPasswordVisible) R.drawable.ic_password_hide else R.drawable.ic_password_show),
+					contentDescription = stringResource(id = R.string.action_togglePasswordVisiblitiy)
+				)
+			}
+		},
+		visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+		label = { Text(text = stringResource(id = R.string.hint_password)) },
+		onValueChange = onValueChange
+	)
+}
+
+@Composable
+private fun ScreenTitle(@StringRes id: Int) {
+	Row(
+		modifier = Modifier.fillMaxWidth(),
+		horizontalArrangement = Arrangement.Center
+	) {
+		Text(
+			text = stringResource(id),
+			style = typography.h4
 		)
 	}
 }
