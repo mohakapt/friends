c4429c5fdea394983ddca119c931b76b7a7008ec
Haytham KATBY <mohakapt@gmail.com>
2023-01-12 17:59:16 +0300

    Setup sign up screen

---
+++ app/src/main/java/com/github/mohaka/friends/signup/SignUpScreen.kt
@@ -0,0 +1,19 @@
+package com.github.mohaka.friends.signup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.mohaka.friends.R
+
+@Composable
+@Preview(device = Devices.PIXEL_4)
+fun SignUpScreen() {
+	Column(modifier = Modifier.fillMaxSize()) {
+	}
+}
