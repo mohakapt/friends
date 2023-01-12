ce12874811254ec0171082b48bcb15a29abcb88d
Haytham KATBY <mohakapt@gmail.com>
2023-01-12 19:46:11 +0300

    Add test for different invalid emails

--- app/src/test/java/com/github/mohaka/friends/signup/CredentialsValidationTest.kt
+++ app/src/test/java/com/github/mohaka/friends/signup/CredentialsValidationTest.kt
@@ -3,15 +3,28 @@ package com.github.mohaka.friends.signup
 import com.github.mohaka.friends.InstantTaskExecuteExtension
 import com.github.mohaka.friends.signup.state.SignUpState
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @ExtendWith(InstantTaskExecuteExtension::class)
 class CredentialsValidationTest {
-	@Test
-	fun invalidEmail() {
+
+	@ParameterizedTest
+	@CsvSource(
+		"'email'",
+		"'a@b.c'",
+		"'ab@b.c'",
+		"'a@bc.c'",
+		"'ab@bc.c'",
+		"'ab@bc'",
+		"'ab.bc'",
+		"''",
+		"'       '",
+	)
+	fun invalidEmail(email: String) {
 		val viewModel = SignUpViewModel()
-		viewModel.createAccount("email", ":password:", ":about:")
+		viewModel.createAccount(email, ":password:", ":about:")
 		assertEquals(SignUpState.BadEmail, viewModel.signUpState.value)
 	}
 }
