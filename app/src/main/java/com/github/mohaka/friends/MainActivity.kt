a68ac35d5cfb71db9a299d42da2e59c65639b011
Haytham KATBY <mohakapt@gmail.com>
2023-01-12 17:58:36 +0300

    Rollback to material theme instead of material3

--- app/src/main/java/com/github/mohaka/friends/MainActivity.kt
+++ app/src/main/java/com/github/mohaka/friends/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.mohaka.friends.ui.theme.FriendsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +14,8 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			FriendsTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 					Greeting("Android")
+				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
 				}
 			}
 		}
