be269d214a9eb7445a0e2deb7b0d0038f95a5eff
Haytham KATBY <mohakapt@gmail.com>
2023-01-12 20:13:58 +0300

    Refactor credentials validator

--- app/src/main/java/com/github/mohaka/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/com/github/mohaka/friends/signup/SignUpViewModel.kt
@@ -10,14 +10,28 @@ class SignUpViewModel {
 	val signUpState: LiveData<SignUpState> = _mutableSignUpState
 
 	fun createAccount(email: String, password: String, about: String) {
-		val emailRegex = """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-		val passwordRegex = """""${'"'}^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#${'$'}%^&*+=\-]).{8,}${'$'}""${'"'}"""
+		val result = validateCredentials(email, password)
 
-		if (!Pattern.compile(emailRegex).matcher(email).matches()) {
-			_mutableSignUpState.value = SignUpState.BadEmail
-		} else if (!Pattern.compile(passwordRegex).matcher(password).matches()) {
-			_mutableSignUpState.value = SignUpState.BadPassword
+		_mutableSignUpState.value = when (result) {
+			CredentialsValidationResult.InvalidEmail -> SignUpState.BadEmail
+			CredentialsValidationResult.InvalidPassword -> SignUpState.BadPassword
 		}
 	}
 
+	private fun validateCredentials(email: String, password: String): CredentialsValidationResult {
+		val emailRegex = """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+		val passwordRegex = """""${'"'}^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#${'$'}%^&*+=\-]).{8,}${'$'}""${'"'}"""
+
+		val result = when {
+			!Pattern.compile(emailRegex).matcher(email).matches() -> CredentialsValidationResult.InvalidEmail
+			!Pattern.compile(passwordRegex).matcher(password).matches() -> CredentialsValidationResult.InvalidPassword
+			else -> TODO()
+		}
+		return result
+	}
+}
+
+sealed class CredentialsValidationResult {
+	object InvalidEmail : CredentialsValidationResult()
+	object InvalidPassword : CredentialsValidationResult()
 }
