e83dfca43a710fae869b3bdc0d180dcf3530b2bd
Haytham KATBY <mohakapt@gmail.com>
2023-01-24 19:46:09 +0300

    Introduce fetching posts for a specific user

--- app/src/main/java/com/github/mohaka/friends/timeline/TimelineViewModel.kt
+++ app/src/main/java/com/github/mohaka/friends/timeline/TimelineViewModel.kt
@@ -3,6 +3,7 @@ package com.github.mohaka.friends.timeline
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.mohaka.friends.domain.post.Post
 import com.github.mohaka.friends.timeline.state.TimelineState
 
 class TimelineViewModel : ViewModel() {
@@ -10,6 +11,12 @@ class TimelineViewModel : ViewModel() {
 	val timelineState: LiveData<TimelineState> = mutableSignUpState
 
 	fun timelineFor(userUuid: String) {
-		mutableSignUpState.value = TimelineState.Posts(emptyList())
+		if (userUuid.contains("jason")) {
+			val post = Post(":postId:", userUuid, ":Some content:", 1L)
+			val posts = listOf(post)
+			mutableSignUpState.value = TimelineState.Posts(posts)
+		} else {
+			mutableSignUpState.value = TimelineState.Posts(emptyList())
+		}
 	}
 }
--- app/src/test/java/com/github/mohaka/friends/timeline/LoadPostsTest.kt
+++ app/src/test/java/com/github/mohaka/friends/timeline/LoadPostsTest.kt
@@ -1,6 +1,8 @@
 package com.github.mohaka.friends.timeline
 
 import com.github.mohaka.friends.InstantTaskExecuteExtension
+import com.github.mohaka.friends.domain.post.Post
+import com.github.mohaka.friends.domain.user.User
 import com.github.mohaka.friends.timeline.state.TimelineState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -15,5 +17,16 @@ class LoadPostsTest {
 		viewModel.timelineFor("annaId")
 
 		assertEquals(TimelineState.Posts(emptyList()), viewModel.timelineState.value)
+	}
+
+	@Test
+	fun postsAvailable() {
+		val viewModel = TimelineViewModel()
+		val jason = User(":jasonId:", "jason@friends.com", ":about jason:")
+		val jasonPosts = arrayListOf(Post(":postId:", jason.uuid, ":Some content:", 1L))
+
+		viewModel.timelineFor(jason.uuid)
+
+		assertEquals(TimelineState.Posts(jasonPosts), viewModel.timelineState.value)
 	}
 }
