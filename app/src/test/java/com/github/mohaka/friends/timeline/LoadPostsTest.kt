6a0b78f11bae193dde6909e76ef829b74b41c51b
Haytham KATBY <mohakapt@gmail.com>
2023-02-05 17:41:05 +0300

    Extract availablePosts to constructor param

--- app/src/test/java/com/github/mohaka/friends/timeline/LoadPostsTest.kt
+++ app/src/test/java/com/github/mohaka/friends/timeline/LoadPostsTest.kt
@@ -14,7 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 class LoadPostsTest {
 	@Test
 	fun noPostsAvailable() {
-		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog())
+		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog(listOf(
+			Post("postId", "timId", "Some content", 1L),
+			Post("post2", "lucyId", "Content of post 2", 2L),
+			Post("post1", "lucyId", "Content of post 1", 1L),
+			Post("post4", "saraId", "Content of post 4", 4L),
+			Post("post3", "saraId", "Content of post 3", 3L),
+		)))
 
 		viewModel.timelineFor("topId")
 
@@ -23,7 +29,13 @@ class LoadPostsTest {
 
 	@Test
 	fun postsAvailable() {
-		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog())
+		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog(listOf(
+			Post("postId", "timId", "Some content", 1L),
+			Post("post2", "lucyId", "Content of post 2", 2L),
+			Post("post1", "lucyId", "Content of post 1", 1L),
+			Post("post4", "saraId", "Content of post 4", 4L),
+			Post("post3", "saraId", "Content of post 3", 3L),
+		)))
 		val tim = aUser().withUuid("timId").build()
 		val timPosts = arrayListOf(Post("postId", tim.uuid, "Some content", 1L))
 
@@ -41,7 +53,13 @@ class LoadPostsTest {
 			Post("post1", lucy.uuid, "Content of post 1", 1L),
 		)
 
-		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog())
+		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog(listOf(
+			Post("postId", "timId", "Some content", 1L),
+			Post("post2", "lucyId", "Content of post 2", 2L),
+			Post("post1", "lucyId", "Content of post 1", 1L),
+			Post("post4", "saraId", "Content of post 4", 4L),
+			Post("post3", "saraId", "Content of post 3", 3L),
+		)))
 		viewModel.timelineFor(anna.uuid)
 
 		assertEquals(TimelineState.Posts(lucyPosts), viewModel.timelineState.value)
@@ -61,7 +79,13 @@ class LoadPostsTest {
 			Post("post3", sara.uuid, "Content of post 3", 3L),
 		)
 
-		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog())
+		val viewModel = TimelineViewModel(InMemoryUserCatalog(), InMemoryPostCatalog(listOf(
+			Post("postId", "timId", "Some content", 1L),
+			Post("post2", "lucyId", "Content of post 2", 2L),
+			Post("post1", "lucyId", "Content of post 1", 1L),
+			Post("post4", "saraId", "Content of post 4", 4L),
+			Post("post3", "saraId", "Content of post 3", 3L),
+		)))
 		viewModel.timelineFor(sara.uuid)
 		assertEquals(TimelineState.Posts(lucyPosts + saraPosts), viewModel.timelineState.value)
 	}
